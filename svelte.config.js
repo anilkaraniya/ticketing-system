226a5a159847c711b5609d4ae8d4c9a4a4757164
Anil Karaniya <anilkaraniya21@gmail.com>
2024-04-01 18:15:28 +0530

    _

--- svelte.config.js
+++ svelte.config.js
@@ -20,6 +20,20 @@ export default {
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
+    prerender: {
+      handleHttpError: ({ path, referrer, message }) => {
+        // ignore deliberate link to shiny 404 page
+        if (
+          path === "/not-found" &&
+          referrer === "/blog/how-we-built-our-404-page"
+        ) {
+          return;
+        }
+
+        // otherwise fail the build
+        throw new Error(message);
+      },
+    },
     // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
     // If your environment is not supported or you settled on a specific environment, switch out the adapter.
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
